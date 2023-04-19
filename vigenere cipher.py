b3b429de0c888c0a66899636708da44789985c0c
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-04-19 22:52:36 +0800

    pseudocode

---
+++ vigenere cipher.py
@@ -0,0 +1,16 @@
+# Assignment 2 PROBLEM 3 The Vigenère Cipher
+# Phoebe Rhone L. Gangoso | BSCPE 1-4
+
+#PSEUDOCODE
+
+# ask the user to input message and key
+# convert message and key to uppercase letters
+
+# create a function to encrypt the message
+#   iterate every character in the message
+#       if character is an alphabet, encrypt with key character
+#       if character is not an alphabet, append as it is
+#   return encrypted message
+
+# encrypt message with key using the function
+# print output ciphertext
